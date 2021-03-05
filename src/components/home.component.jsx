b407de381b516d5df24864288c26af61276833f2
Muneeb Hussain Modi <muneebhussainmodi@gmail.com>
2021-03-05 15:03:43 +0500

    New components added

---
+++ src/components/home.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home(props) {
+  return (
+    <div className="">
+      Hi This is {props.title} and {props.myName}
+    </div>
+  );
+}
+
+export default Home;
