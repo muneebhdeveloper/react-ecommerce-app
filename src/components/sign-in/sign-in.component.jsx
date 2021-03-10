6401d18d019f6012be627756d29e46de29ed0b4d
Muneeb Hussain Modi <muneebhussainmodi@gmail.com>
2021-03-10 17:18:43 +0500

    Add FormInput Component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
+import FormInput from "../form-input/form-input.component";
 import "./sign-in.styles.scss";
 
-const SignIn = () => (
+class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
 
-);
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ email: "", password: "" });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h1>I already have an account</h1>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            type="email"
+            name="email"
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label="Email"
+            required
+          />
+          <FormInput
+            type="password"
+            name="password"
+            handleChange={this.handleChange}
+            value={this.state.password}
+            label="Password"
+            required
+          />
+          <input type="submit" value="Submit Form" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import SignIn from "../../components/sign-in/sign-in.component";
 import "./sign-in-and-sign-up.styles.scss";
 
 const SignInAndSignUp = () => (
-  <div className="sign-in-and-sign-up">Sign In</div>
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
 );
 
 export default SignInAndSignUp;
