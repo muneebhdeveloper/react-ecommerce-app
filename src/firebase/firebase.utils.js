d7a63f335061201e6eee9fb56beec8ec8ccf0712
Muneeb Hussain Modi <muneebhussainmodi@gmail.com>
2021-03-27 23:08:07 +0500

    Added Sign Functionality and Sign Up Component with Functionality

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -15,8 +15,35 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+// Checking User in the Database
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const userSnapshot = await userRef.get();
+
+  if (!userSnapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("There is an error ", error.message);
+    }
+  }
+
+  return userRef;
+};
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
