d0de146374cf199c21ee8bdae4dd0db4dd03c1bd
Muneeb Hussain Modi <muneebhussainmodi@gmail.com>
2021-05-15 03:30:59 +0500

    Reduxify Directory and Shop Components

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,20 @@
-import React, { Component } from "react";
-import SHOP_DATA from "./shop-data";
+import React from "react";
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectShopCollections } from "../../redux/shop/shop.selectors";
 import "./shop.styles.scss";
 
-class Shop extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="">
-        {collections.map(({ id, ...otherShopProps }) => (
-          <CollectionPreview key={id} {...otherShopProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const Shop = ({ collections }) => (
+  <div className="">
+    {collections.map(({ id, ...otherShopProps }) => (
+      <CollectionPreview key={id} {...otherShopProps} />
+    ))}
+  </div>
+);
 
-export default Shop;
+const mapStateToProps = createStructuredSelector({
+  collections: selectShopCollections,
+});
+
+export default connect(mapStateToProps)(Shop);
